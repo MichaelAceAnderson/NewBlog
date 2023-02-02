f9bdb705d3b375d265e94dafde2a168106647f4e
Michael Anderson <adrien.spy@gmail.com>
2023-02-02 16:28:45 +0100

    NewBlog 1.0 commit

---
+++ admin/errors/403.php
@@ -0,0 +1,5 @@
+<?php include($_SERVER["DOCUMENT_ROOT"].'/admin/includes/head.php')?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/admin/includes/header.php') ?>
+<?php
+echo '<h1 style="color: red;">Accès au dossier interdit !</h1>'; ?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/admin/includes/footer.php') ?>
---
+++ admin/errors/404.php
@@ -0,0 +1,5 @@
+<?php include($_SERVER["DOCUMENT_ROOT"].'/admin/includes/head.php')?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/admin/includes/header.php')?>
+<?php
+echo '<h1 style="color: red;">Ce que vous cherchez est introuvable... retournez a l\'accueil !</h1>'; ?>
+<?php include($_SERVER["DOCUMENT_ROOT"].'/admin/includes/footer.php')?>
