117103fc36635a1eee56b5c3578175916447cb65
Michael Anderson <adrien.spy@gmail.com>
2023-03-29 15:13:33 +0200

    Style tweaks

--- view/includes/content/login.php
+++ view/includes/content/login.php
@@ -1,19 +1,21 @@
 <div class="main">
     <div class="content">
-        <div class="post">
+        <div class="panel">
             <h1>Connectez vous:</h1>
-            <?php
-            if (isset($formError)) {
-                echo '<h1 class="notification error">' . $formError . '</h1>';
-            }
-            ?>
-            <form method="post" action="">
-                <label for="fUsername">Identifiant:</label>
-                <input type="text" name="fUserName" required />
-                <label for="fUsername">Mot de passe:</label>
-                <input type="password" name="fPass" required />
-                <input type="submit" value="Connexion" name="fLogin" />
-            </form>
+            <div class="panel-content">
+                <?php
+                if (isset($formError)) {
+                    echo '<h1 class="notification error">' . $formError . '</h1>';
+                }
+                ?>
+                <form method="post" action="">
+                    <label for="fUsername">Identifiant:</label>
+                    <input type="text" name="fUserName" placeholder="Nom d'utilisateur" required />
+                    <label for="fUsername">Mot de passe:</label>
+                    <input type="password" name="fPass" placeholder="Mot de passe" required />
+                    <input type="submit" value="Connexion" name="fLogin" />
+                </form>
+            </div>
         </div>
     </div>
 </div>
